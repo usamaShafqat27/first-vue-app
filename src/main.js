e8654bc3ea0dd448055928e197c89cbfb65a7c94
usama.shafqat <usama.shafqat95@gmail.com>
2021-02-18 18:46:06 +0500

    Experiment with directives

--- src/main.js
+++ src/main.js
@@ -4,3 +4,22 @@ var app = new Vue({
 		message: "Hello From Vue!",
 	},
 });
+
+var app2 = new Vue({
+	el: "#app2",
+	data: {
+		message: "Just some tooltip info",
+	},
+});
+
+var app3 = new Vue({
+	el: "#app3",
+	data: {
+		todos: [
+			"Make a repo",
+			"Connect Vue via CDN",
+			"Make your first element",
+			"Play around with vue directives",
+		],
+	},
+});
